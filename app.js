ee66e4eae6f78bd4c7070b54eca8178fef1b5220
Pramod <pramodraina047@gmail.com>
2022-11-03 12:01:59 +0000

    Initialized DB and Server

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const addDays = require("date-fns/addDays");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+const app = express();
+app.use(express.json());
+
+const dbPath = path.join(__dirname, "todoApplication.db");
+
+let db = null;
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server is running at http://localhost:3000/");
+    });
+  } catch (err) {
+    console.log(`DB Error: ${err.message}`);
+    process.exit(-1);
+  }
+};
+
+initializeDBAndServer();
